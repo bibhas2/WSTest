e887f1f5326997256717e22f3ec408e83a7bf3c3
Bibhas Bhattacharya <bibhas2@gmail.com>
2014-05-20 10:03:59 -0400

    Initial release

---
+++ src/com/mobiarch/ws/Broadcaster.java
@@ -0,0 +1,20 @@
+package com.mobiarch.ws;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Broadcaster
+ */
+@WebServlet("/broadcast")
+public class Broadcaster extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		SimpleWSHandler.broadcastMessage("Hello World");
+	}
+}
